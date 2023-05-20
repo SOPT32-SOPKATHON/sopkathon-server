5fae2013fa2ea60f3485f012e109089a51e9155a
ddongseop <dlehdtjq00o@gmail.com>
2023-05-20 18:47:16 +0900

    Initial Commit

---
+++ src/test/java/sopt/org/sopkathon/ApplicationTests.java
@@ -0,0 +1,13 @@
+package sopt.org.sopkathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

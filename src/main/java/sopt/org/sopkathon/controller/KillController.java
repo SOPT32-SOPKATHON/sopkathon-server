850eb86911cf1715c4ee400fdfb2c897a151b8c3
chaerin <achrvvv@gmail.com>
2023-05-21 01:50:38 +0900

    feat: add vote api controller

--- src/main/java/sopt/org/sopkathon/controller/KillController.java
+++ src/main/java/sopt/org/sopkathon/controller/KillController.java
@@ -2,13 +2,11 @@ package sopt.org.sopkathon.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sopt.org.sopkathon.common.dto.ApiResponse;
-import sopt.org.sopkathon.controller.dto.response.RankKillResponseDto;
 import sopt.org.sopkathon.controller.dto.response.KillResponseDto;
+import sopt.org.sopkathon.controller.dto.response.RankKillResponseDto;
+import sopt.org.sopkathon.controller.score.dto.EditKillRankScoreRequestDto;
 import sopt.org.sopkathon.exception.Success;
 import sopt.org.sopkathon.service.KillService;
 
@@ -31,5 +29,13 @@ public class KillController {
     @ResponseStatus(HttpStatus.OK)
     public ApiResponse<List<RankKillResponseDto>> getRankKillList() {
         return ApiResponse.success(Success.GET_KILL_LIST_SUCCESS, killService.getRankKillList());
+    }
+
+    @PatchMapping("/{killId}/vote")
+    @ResponseStatus(HttpStatus.OK)
+    public void editRankScore(@PathVariable final Long killId, @RequestBody EditKillRankScoreRequestDto request) {
+        System.out.println(killId);
+        System.out.println(request);
+
     }
 }
---
+++ src/main/java/sopt/org/sopkathon/controller/score/dto/EditKillRankScoreRequestDto.java
@@ -0,0 +1,13 @@
+package sopt.org.sopkathon.controller.score.dto;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class EditKillRankScoreRequestDto {
+    private String type;
+
+
+}
